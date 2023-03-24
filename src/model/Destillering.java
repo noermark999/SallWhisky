e84078ac513376da278029cac59ad14453cff2e7
holml <holmloch@gmail.com>
2023-03-24 11:49:38 +0100

    yo

--- src/model/Destillering.java
+++ src/model/Destillering.java
@@ -1,4 +1,24 @@
 package model;
 
+import java.util.Date;
+
 public class Destillering {
+    private Date startDato;
+    private Date slutDato;
+    private String maltBatch;
+    private String kornsort;
+    private String medarbejder;
+    private int maengde;
+    private double alkoholProcent;
+
+    public Destillering(Date startDato, Date slutDato, String maltBatch, String kornsort, String medarbejder, int maengde, double alkoholProcent) {
+        this.startDato = startDato;
+        this.slutDato = slutDato;
+        this.maltBatch = maltBatch;
+        this.kornsort = kornsort;
+        this.medarbejder = medarbejder;
+        this.maengde = maengde;
+        this.alkoholProcent = alkoholProcent;
+    }
+
 }
