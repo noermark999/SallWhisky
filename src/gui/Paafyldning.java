fa3a7c90ddb7bd502be6718a5888a9bb31562225
Noermark999 <jakobnoermark@hotmail.com>
2023-03-27 08:42:56 +0200

    Paafyldning

--- src/gui/Paafyldning.java
+++
@@ -1,9 +0,0 @@
-package gui;
-
-import java.util.Date;
-
-public class Paafyldning {
-    private int maengde;
-    private Date datoForPaafyldning;
-
-}
---
+++ src/model/Paafyldning.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.util.Date;
+
+public class Paafyldning {
+    private int maengde;
+    private Date datoForPaafyldning;
+    private Fad fad;
+
+    public Paafyldning(int maengde, Date datoForPaafyldning, Fad fad) {
+        this.maengde = maengde;
+        this.datoForPaafyldning = datoForPaafyldning;
+        this.fad = fad;
+    }
+
+    public int getMaengde() {
+        return maengde;
+    }
+
+    public Date getDatoForPaafyldning() {
+        return datoForPaafyldning;
+    }
+
+    public Fad getFad() {
+        return fad;
+    }
+}
